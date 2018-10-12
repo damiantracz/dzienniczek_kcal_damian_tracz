76b9c36a0f01e1cc3feb3e841ba1a549cf6203bb
Danio-MSI <danio-msi@d1t.ayz.pl>
2018-10-12 21:56:02 +0200

    Utworzenie projektu.

---
+++ src/main/java/damian/kcal/dzienniczek/DzienniczekApplication.java
@@ -0,0 +1,12 @@
+package damian.kcal.dzienniczek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzienniczekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DzienniczekApplication.class, args);
+    }
+}
