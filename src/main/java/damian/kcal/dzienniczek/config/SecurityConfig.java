6946d3121511935662abe7496ef88b29c9ebb76c
Danio-MSI <danio-msi@d1t.ayz.pl>
2018-10-14 14:15:14 +0200

    Utworzenie testowego logowania.

---
+++ src/main/java/damian/kcal/dzienniczek/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package damian.kcal.dzienniczek.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/*
+Linki:
+https://docs.spring.io/spring-security/site/docs/current/guides/html5/helloworld-boot.html
+ */
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**", "/index").permitAll()
+                .antMatchers("/user/**").hasRole("USER")
+                .and()
+                .formLogin()
+                .loginPage("/login").failureUrl("/login-error");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER");
+                //{noop} - https://stackoverflow.com/questions/46999940/spring-boot-passwordencoder-error
+    }
+}
--- src/main/java/damian/kcal/dzienniczek/controller/UserController.java
+++ src/main/java/damian/kcal/dzienniczek/controller/UserController.java
@@ -1,14 +1,45 @@
 package damian.kcal.dzienniczek.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UserController {
 
+    /*
     @RequestMapping(value = "/users", method = RequestMethod.GET)
     public String showUsers(){
         return "test";
+    }
+    */
+
+
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/user/index")
+    public String userIndex() {
+        return "user/index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login";
     }
 }
