9aa842c59a2fe352848deb3eda4f2c949ffa8f74
Danio-MSI <danio-msi@d1t.ayz.pl>
2018-10-28 23:15:17 +0100

    Poprawa rejestracji (formularz zawierał błędy(password2))

--- src/main/java/damian/kcal/dzienniczek/model/User.java
+++ src/main/java/damian/kcal/dzienniczek/model/User.java
@@ -10,16 +10,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import java.util.Date;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
 @Entity
@@ -44,6 +35,9 @@ public class User {
 
     @Column(name = "password")
     private String password;
+
+    @Transient
+    private String password2;
 
     @Column(name = "active")
     private int active;
